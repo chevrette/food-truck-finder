fd9920c1da559c10603c518d1ec05ac531802d19
Karolina Denkiewicz <karolina@Damians-MacBook-Pro.local>
2022-04-10 00:30:57 +0100

    Improve fetching user's location

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import { useCallback, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from './context/locationContext';
+import { getClosestPlaces } from './services/places';
+import { loadingStatus } from './constants';
 import { defaultLocation } from './constants';
 import Places from './components/Places';
+import Button from '@mui/material/Button';
 import './App.scss';
+
+const placesNum = 10;
 
 const App = () => {
   const { location, setLocation } = useLocation();
 
-  const getLocation = useCallback(() => {
+  const [places, setPlaces] = useState([]);
+  const [loading, setLoading] = useState(loadingStatus.NOT_STARTED);
+
+  useEffect(() => {
+    const getPlaces = async () => {
+      try {
+        const closestPlaces = await getClosestPlaces(location, placesNum);
+        setPlaces(closestPlaces);
+        setLoading(loadingStatus.READY);
+      } catch {
+        setLoading(loadingStatus.FAILED);
+      }
+    };
+    if (location) getPlaces();
+  }, [location]);
+
+  const getLocation = () => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
         const userLocation = {
@@ -18,13 +39,21 @@ const App = () => {
       },
       () => setLocation(defaultLocation),
     );
-  }, [setLocation]);
+  };
 
-  useEffect(() => {
+  const getClosestFoodTrucks = async () => {
+    setLoading(loadingStatus.IN_PROGRESS);
     getLocation();
-  }, [getLocation]);
+  };
 
-  return <div className="App">{location && <Places />}</div>;
+  return (
+    <div className="App">
+      <Button onClick={getClosestFoodTrucks} variant="contained">
+        Find closest foodtrucks
+      </Button>
+      <Places loading={loading} places={places} />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Places.js
+++ src/components/Places.js
@@ -1,34 +1,12 @@
-import { useState } from 'react';
-import { useLocation } from './../context/locationContext';
-import { getClosestPlaces } from './../services/places';
 import { loadingStatus } from './../constants';
 import PlacesTable from './../components/PlacesTable';
-import Button from '@mui/material/Button';
 
-const placesNum = 10;
-
-const Places = () => {
-  const { location } = useLocation();
-
-  const [places, setPlaces] = useState([]);
-  const [loading, setLoading] = useState(loadingStatus.NOT_STARTED);
-
-  const getClosestFoodTrucks = async () => {
-    try {
-      setLoading(loadingStatus.IN_PROGRESS);
-      const closestPlaces = await getClosestPlaces(location, placesNum);
-      setPlaces(closestPlaces);
-      setLoading(loadingStatus.READY);
-    } catch {
-      setLoading(loadingStatus.FAILED);
-    }
-  };
-
+const Places = ({ loading, places }) => {
   return (
     <div>
-      <Button onClick={getClosestFoodTrucks} variant="contained">
-        Find closest foodtrucks
-      </Button>
+      {loading == loadingStatus.NOT_STARTED && (
+        <p>Click button to find food trucks in your location</p>
+      )}
       {loading === loadingStatus.IN_PROGRESS && <p>Loading...</p>}
       {loading === loadingStatus.FAILED && (
         <p>Sorry, we can't get food trucks' list at this moment</p>
